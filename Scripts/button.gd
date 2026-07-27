6b827aae4cb15e23a7e65fdc9192d3c570e659f6
Crimtane <said-h95@hotmail.com>
2026-07-27 18:43:21 +0200

    Finished button-box interaction
    Just missing art

--- Scripts/button.gd
+++ Scripts/button.gd
@@ -1,15 +1,20 @@
 extends Area2D
 
+var count = 0
 var entered = false
 
 
-func _on_body_entered(_body: RigidBody2D) -> void:
-	if not entered:
-		entered = true
-		print("entered")
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Boxes"):
+		count += 1
+		if not entered && count >= 1:
+			entered = true
+			print("entered")
 
 
-func _on_body_exited(_body: RigidBody2D) -> void:
-	if entered:
-		entered = false
-		print("exited")
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Boxes"):
+		count -= 1
+		if entered && count < 1:
+			entered = false
+			print("exited")
