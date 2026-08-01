29b008c043f751a2b03ecf1305452f2c265c195e
Crimtane <said-h95@hotmail.com>
2026-08-01 22:33:39 +0200

    More power to da buttons
    - Made it so that buttons can open/close multiple doors.
    - Doors can now also be inverted (starts in the open position).
    - Placed tiles and objects for the next area.

--- Scripts/button.gd
+++ Scripts/button.gd
@@ -3,21 +3,27 @@ extends Area2D
 signal button_pressed(door, closedPosition, openedPosition)
 signal button_unpressed(door, closedPosition, openedPosition)
 
-@export var door: AnimatableBody2D
-@onready var closedPosition = door.position.y
-@onready var openedPosition = door.position.y-128
+@export var doors: Array[AnimatableBody2D]
+var doorPositions: Array[Vector2]
 
 var count = 0
 var entered = false
 
+func _ready() -> void:
+	for door in doors:
+		doorPositions.append(Vector2(door.position.y, door.position.y-128))
+		if door.inverted:
+			door.position.y -= 128
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Boxes"):
 		count += 1
 		if not entered && count >= 1:
 			entered = true
-			button_pressed.emit(door, closedPosition, openedPosition)
 			$AnimatedSprite2D.frame = 1
+			for door in doors:
+				var i = doors.find(door)
+				button_pressed.emit(door, doorPositions[i].x, doorPositions[i].y)
 
 
 func _on_body_exited(body: Node2D) -> void:
@@ -25,5 +31,7 @@ func _on_body_exited(body: Node2D) -> void:
 		count -= 1
 		if entered && count < 1:
 			entered = false
-			button_unpressed.emit(door, closedPosition, openedPosition)
 			$AnimatedSprite2D.frame = 0
+			for door in doors:
+				var i = doors.find(door)
+				button_unpressed.emit(door, doorPositions[i].x, doorPositions[i].y)
--- Scripts/door.gd
+++ Scripts/door.gd
@@ -1,17 +1,26 @@
 extends AnimatableBody2D
 
+@export var inverted: bool
 var opened = false
 
 
-func _on_button_pressed(door, _closedPosition, openedPosition) -> void:
-	var tween = create_tween()
+func _on_button_pressed(door, closedPosition, openedPosition) -> void:
 	if not opened && door == self:
 		opened = true
-		tween.tween_property(self,"position:y", openedPosition, 0.5)
+		if not inverted:
+			var tween = create_tween()
+			tween.tween_property(self,"position:y", openedPosition, 0.5)
+		else:
+			var tween = create_tween()
+			tween.tween_property(self,"position:y", closedPosition, 0.5)
 
 
-func _on_button_unpressed(door, closedPosition, _openedPosition) -> void:
-	var tween = create_tween()
+func _on_button_unpressed(door, closedPosition, openedPosition) -> void:
 	if opened && door == self:
 		opened = false
-		tween.tween_property(self,"position:y", closedPosition, 0.5)
+		if not inverted:
+			var tween = create_tween()
+			tween.tween_property(self,"position:y", closedPosition, 0.5)
+		else:
+			var tween = create_tween()
+			tween.tween_property(self,"position:y", openedPosition, 0.5)
