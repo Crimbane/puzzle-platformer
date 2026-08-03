637561d1311f0737e24ce678d6b65c769def74f2
olem50 <olem50@hotmail.com>
2026-08-03 19:45:11 +0200

    Bloody spikes and other sprites
    Removed the open animation from lock
    Added platform, lever sprites.

--- Scripts/spikes.gd
+++ Scripts/spikes.gd
@@ -1,10 +1,28 @@
 extends Area2D
 
+var randomNumber = RandomNumberGenerator.new()
+var spriteNumberSelector
+
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
+	spriteNumberSelector = randomNumber.randi_range(1, 2)
 
 func _on_body_entered(body: Node2D) -> void:
 	# Check if the thing that touched the spike is the Player
 	if body.is_in_group("Player"):
 		# Trigger the hit signal directly on the player
 		body.hit.emit() 
+		bloodSpikes()
+
+func bloodSpikes() -> void:
+	if $AnimatedSprite2D.animation == "blood":
+			$AnimatedSprite2D.animation = "big blood"
+			return
+	elif $AnimatedSprite2D.animation == "blood2":
+		$AnimatedSprite2D.animation = "big blood 2"
+		return
+		
+	if spriteNumberSelector == 1:
+		$AnimatedSprite2D.animation = "blood"
+	elif spriteNumberSelector == 2:
+		$AnimatedSprite2D.animation = "blood2"
