e53beeb38ab6de19e6683c447e6d4f8b25c53790
Crimtane <said-h95@hotmail.com>
2026-08-06 19:33:55 +0200

    Bug fixes and stuff
    - Fixed an issue where portals would grow bigger than their original size during cooldown animation.
    
    - Added a platform on the elevator that you can jump up to or go down from (S or Down arrow).
    
    - Removed redundant foreground tiles and replaced them with background tiles (saves FPS cuz less collision to calculate).
    
    - Started the path to a secret area above the elevator shaft.

--- Scripts/portal.gd
+++ Scripts/portal.gd
@@ -38,7 +38,7 @@ func _on_body_entered(body: Node2D) -> void:
 		tween.tween_property(self, "scale", Vector2(), 0.01) # self or $AnimatedSprite2D works
 		await get_tree().create_timer(0.01).timeout
 		resetTween()
-		tween.tween_property(self, "scale", Vector2(1,1), teleportCooldown).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+		tween.tween_property(self, "scale", Vector2(.8,.8), teleportCooldown).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 		
 		await get_tree().create_timer(teleportCooldown).timeout
 		
